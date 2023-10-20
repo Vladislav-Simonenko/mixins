dc2b699693aa56a82fa32ef980fdd1387420bca5
Vladislav Simonenko <vlad.simonenko@itp-it.com>
2023-10-20 16:23:52 +0400

    bugfix(mixins) MX-1. fix button text

--- src/components/Alignment/Alignment.tsx
+++ src/components/Alignment/Alignment.tsx
@@ -3,7 +3,22 @@ import styles from "./Alignment.module.scss";
 export const Alignment = () => {
   return (
     <div className={styles.alignmentContainer}>
-      <div className={styles.alignment}> @include center;</div>
+      <div className={styles.alignment}>{`@mixin format-text(
+  $font-family,
+  $font-size,
+  $font-weight,
+  $text-color,
+  $line-height,
+  $text-align
+) {
+  font-family: $font-family;
+  font-size: $font-size;
+  font-weight: $font-weight;
+  color: $text-color;
+  line-height: $line-height;
+  text-align: $text-align;
+}
+`}</div>
     </div>
   );
 };
